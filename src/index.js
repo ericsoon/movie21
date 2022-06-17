4a523a960738b4ee09fb5d169909b6962a0d0918
ericsoon <34089934+ericsoon@users.noreply.github.com>
2022-06-17 16:39:50 -0700

    fetched API from TMDB website using redux-toolkit

--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material';
 
+import { Provider } from 'react-redux';
+
 import App from './components/App';
+import store from './app/store';
 
 const theme = createTheme({});
 
 ReactDom.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
   document.getElementById('root'),
 );
