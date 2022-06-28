1c0f3aeac167427c8eecab58a86a1517925e8624
Eric Soon <eric.soonsmk@gmail.com>
2022-06-28 14:51:46 -0700

    finishing the authentication (added the logout button)

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+import { Typography, Button, Box } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
+
+import { userSelector } from '../../features/auth';
 
 const Profile = () => {
-  console.log('Profile');
+  const { user } = useSelector(userSelector);
+  const logout = () => {
+    localStorage.clear();
+
+    window.location.href = '/';
+  };
+
+  const favoriteMovies = {};
   return (
-    <div>Profile</div>
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>My Profile</Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length
+        ? (
+          <Typography variant="h5">
+            Add Favorites or watchlist some movies to see them here!
+          </Typography>
+        )
+        : (
+          <Box>
+            Favorite Movie
+          </Box>
+        )}
+    </Box>
   );
 };
 
