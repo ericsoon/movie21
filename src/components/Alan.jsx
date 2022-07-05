ecdb844ddad77c71b831be13b0ca62e8b6067a20
Eric Soon <eric.soonsmk@gmail.com>
2022-07-04 21:40:01 -0700

    finished the AI commands

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,17 +1,33 @@
 import { useEffect, useContext } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 import { ColorModeContext } from '../utils/ToggleColorMode';
 import { fetchToken } from '../utils';
+import { selectGenreOrCategory, searchMovie } from '../features/currentGenreOrCategory';
 
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
 
+  const dispatch = useDispatch();
+  const history = useHistory();
+
   useEffect(() => {
     alanBtn({
       key: 'e20897cc51127002ea574afd86eb98672e956eca572e1d8b807a3e2338fdd0dc/stage',
-      onCommand: ({ command, mode }) => {
-        if (command === 'changeMode') {
+      onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+        if (command === 'chooseGenre') {
+          const foundGenre = genres.find((g) => g.name.toLowerCase() === genreOrCategory.toLowerCase());
+          if (foundGenre) {
+            history.push('/');
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else {
+            const category = genreOrCategory.startsWith('top') ? 'top_rated' : genreOrCategory;
+            history.push('/');
+            dispatch(selectGenreOrCategory(category.id));
+          }
+        } else if (command === 'changeMode') {
           if (mode === 'light') {
             setMode('light');
           } else {
@@ -23,6 +39,8 @@ const useAlan = () => {
           localStorage.clear();
 
           window.location.href = '/';
+        } else if (command === 'search') {
+          dispatch(searchMovie(query));
         }
       },
     });
